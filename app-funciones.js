bfbb60b49ff6d777dc48028dc2e4a9f57584bb42
Keimer <Keimerrzc@gmail.com>
2018-10-07 20:53:21 -0400

    cambios de funciones flecha

--- app-funciones.js
+++ app-funciones.js
@@ -1,12 +1,30 @@
 "use strict";
 function Activar(obligatorio, porDefecto, opcional) {
     if (porDefecto === void 0) { porDefecto = "Batiseñal"; }
+    var mensaje;
     if (opcional === undefined) {
-        var mensaje_1 = obligatorio + " activ\u00F3 la " + porDefecto;
+        mensaje = obligatorio + " activ\u00F3 la " + porDefecto;
     }
     else {
-        var mensaje_2 = obligatorio + " activ\u00F3 la " + porDefecto + " en la " + opcional;
+        mensaje = obligatorio + " activ\u00F3 la " + porDefecto + " en la " + opcional;
     }
     console.log(mensaje);
 }
 Activar("Pinguino");
+Activar("Keimer", "Alarma", "Mañana bien temprano");
+// Funciones flechas solucionan el problema del uso del this dentro del scope de una función
+var nombre = "Keimer";
+var hulk = {
+    nombre: "Hulk",
+    smash: function () {
+        setTimeout(function () {
+            console.log(nombre + " smash!!!!!");
+        }, 1500);
+    },
+    smashFlecha: function () {
+        var _this = this;
+        setTimeout(function () { return console.log(_this.nombre + " smash Flecha!!!!!"); }, 2000);
+    }
+};
+hulk.smash();
+hulk.smashFlecha();
--- app-funciones.ts
+++ app-funciones.ts
@@ -1,13 +1,32 @@
 function Activar(obligatorio:string,
                   porDefecto:string = "Batiseñal",
                   opcional?:string){
-  
+  let mensaje:string;
   if ( opcional === undefined ){
-    let mensaje = `${ obligatorio } activó la ${ porDefecto }`;
+    mensaje = `${ obligatorio } activó la ${ porDefecto }`;
   }else{
-    let mensaje = `${ obligatorio } activó la ${ porDefecto } en la ${ opcional }`;
+    mensaje = `${ obligatorio } activó la ${ porDefecto } en la ${ opcional }`;
   }
   console.log(mensaje);
 }
 
 Activar("Pinguino");
+Activar("Keimer", "Alarma","Mañana bien temprano");
+
+// Funciones flechas solucionan el problema del uso del this dentro del scope de una función
+let nombre:string = "Keimer";
+
+let hulk = {
+  nombre : "Hulk",
+  smash(){
+    setTimeout(function(){ 
+      console.log( nombre + " smash!!!!!")
+    },1500);
+  },
+  smashFlecha() {
+    setTimeout( () => console.log(this.nombre + " smash Flecha!!!!!"), 2000);
+  }
+}
+
+hulk.smash();
+hulk.smashFlecha();
