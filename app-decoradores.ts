e38d699a27aeae7b94badca97e071ea9b480b052
kzamora <keimer.zamora@ext-security.cl>
2018-10-14 19:55:33 -0300

    Ejercicio decoradores en TS

---
+++ app-decoradores.ts
@@ -0,0 +1,12 @@
+// Decorador es una función normal que adiciona funcionalidad a una clase usando la sintaxis siguiente:
+
+function consola( constructor:Function){
+    console.log( constructor );
+}
+
+@consola
+class Villanos{
+    constructor(public nombre:string){
+
+    }
+}
