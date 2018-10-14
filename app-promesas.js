1244a89d5541f182c675ea200fae2836670bee64
kzamora <keimer.zamora@ext-security.cl>
2018-10-14 13:19:28 -0300

    Ejercicio de Promesas

---
+++ app-promesas.js
@@ -0,0 +1,16 @@
+var prom1 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log("Finalizó ejecución asincrona");
+        //Simula cuando la función asincrona termina BIEN
+        // resolve();
+        //Simula cuando la función asincrona termina con ERRORES
+        reject();
+    }, 1500);
+});
+console.log("Paso1");
+prom1.then(function () {
+    console.log("Ejecutarme cuando termine BIEN la ejecución Asincrona");
+}, function () {
+    console.error("ERROR: Ejecutarme cuando termine -- MAL -- la ejecución Asincrona");
+});
+console.log("Paso2");
