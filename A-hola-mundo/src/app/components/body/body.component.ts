e282813bd9f46de6634a9075979340b6f920b4e7
kzamora <keimer.zamora@ext-security.cl>
2018-10-15 00:26:34 -0300

    Completar el header, Body y footer del ejercicio Hola Mundo

--- A-hola-mundo/src/app/components/body/body.component.ts
+++ A-hola-mundo/src/app/components/body/body.component.ts
@@ -6,8 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.css']
 })
 export class BodyComponent implements OnInit {
+  card: boolean;
+  corazon: boolean;
+  familiares: boolean;
 
-  constructor() { }
+  frase: any = {
+    autor: 'Keimer Zamora',
+    mensaje: 'Eres mi media naranja, mi motor y gasolina que me impulsa a luchar dia a dia, TE AMO BEBE.'
+  };
+
+  // Familiares
+  padres: string[] = ['Mirna Zamora Cisneros', 'Enrrique Zamora Cisneros', 'Tulio Salazar Torres', 'Rosa Salazar Torres'];
+  hijos: string[] = ['Daniel Zamora Salazar', 'Sebastian Zamora Salazar'];
+
+  constructor() { 
+    this.card = true;
+    this.corazon = false;
+    this.familiares = true;
+    // this.familiarPadres = true;
+  }
 
   ngOnInit() {
   }
