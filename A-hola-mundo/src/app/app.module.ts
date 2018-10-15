ed47bbefdc76000cd55fa3ef222e782831a12ac4
kzamora <keimer.zamora@ext-security.cl>
2018-10-14 22:18:42 -0300

    eliminar los componentes header, body y footer y volver a crearlos en otra ruta, instalar bootstrap con npm y ajustar el archivo angular-cli.json para configurar el boostrap

--- A-hola-mundo/src/app/app.module.ts
+++ A-hola-mundo/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/headers/src/app/components/headers/header/header.component';
-import { BodyComponent } from './components/body/src/app/components/body/body/body.component';
-import { FooterComponent } from './components/footer/src/app/components/footer/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
